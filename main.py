8e99a3b30d51683964ad070c7ac0a62646decb65
Akinwande Oladipo <54485470+Akinwande-Mhayormarkdee-Oladipo@users.noreply.github.com>
2024-07-23 11:04:08 +0100

    Quiz Question basis completed

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-class User:
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+def create_question_objects():
+    questions = []
+    for question in question_data:
+        question_text = question['text']
+        answer = question['answer']
+        q = Question(question_text, answer)
+        questions.append(q)
+    return questions
+
+
+question_bank = create_question_objects()
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
